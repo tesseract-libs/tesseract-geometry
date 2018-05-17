d3770163df0388e84d1979d405a3fcc8761873fb
Urban Soban <u.soban@gmail.com>
2018-05-17 20:38:01 +0200

    0.1.1. -> 0.1.2.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TesseractGeometry.MixProject do
   def project do
     [
       app: :tesseract_geometry,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
